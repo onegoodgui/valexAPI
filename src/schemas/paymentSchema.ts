6ae3c4c681ba54149b1ada4225c2e2431425c86f
Liaess <yann.melo@gmail.com>
2022-04-26 16:29:27 -0300

    Preparando revisão de código

--- src/schemas/paymentSchema.ts
+++
@@ -1,8 +0,0 @@
-import joi from "joi";
-
-export const paymentSchema = joi.object({
-
-    amount:joi.number().positive(),
-    password: joi.string().regex(/^[0-9]{4}$/).required(),
-    businessId: joi.number().positive()
-})
--- src/schemas/rechargeSchema.ts
+++
@@ -1,6 +0,0 @@
-import joi from "joi";
-
-export const rechargeCardSchema = joi.object({
-
-    amount:joi.number().positive()
-})
